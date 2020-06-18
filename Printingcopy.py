01e41f18474b54cd96c22ee05dd1d16e976f589f
geetikay <67020141+geetikay@users.noreply.github.com>
2020-06-18 12:58:58 -0700

    new file

---
+++ Printingcopy.py
@@ -0,0 +1,28 @@
+#programming paradigm = style of programming
+    #Python supports procedural programming, functional programming, and object oriented programming
+        #procedural programming = splits instructions into procedures (proedures, unlike functions, do not have any desired resulting value; they simply accomplish a certain task)
+        #functional programming = execution of a series of mathematical functions (do not modify and are not affected by outside values)
+        #object-oriented programming = based on objects, which contain data
+
+#print is one of the Python functions, place text you want printed inside ()
+#When printing strings, use either double or single quotations but stay consistent 
+print ("Hello World")
+print ('Hello World')
+
+#string = text (Python type), ex: "Hello World"
+
+#Use the print function for calculations
+#Do not use quotation marks for numbers
+print (1+2)
+print (7*6)
+#exponentiation
+print (4**2)
+#modulo gives remainder
+print (18%7)
+print ()
+print ("The end")
+
+#When calling a function, must use () after function is named
+
+#Can print several strings + numbers at once, using commas to separate arguments
+print ("The end", "or is it", "keep watching to learn more about Python", 3)
